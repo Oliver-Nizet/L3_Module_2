22b9f7bd6ba6cf8d8459df581fede08f5003b92d
Oliver Nizet <olivernizet@att.net>
2018-07-25 21:07:02 -0700

    dasdf

--- src/intro_to_file_io/BubbleSort.java
+++ src/intro_to_file_io/BubbleSort.java
@@ -3,29 +3,20 @@ package intro_to_file_io;
 public class BubbleSort {
 	public static void main(String[] args) {
 		BubbleSort bs = new BubbleSort();
-		int[] test = { 2, 4, 5, 9, 1, 3 };
-		boolean unsorted = true;
+		int[] test = { 2, 4, 3, 8, 5, 9 };
+		for (int i = 0; i < 10000; i++) {
+			sort(test);
+		}
 		String s = "";
 		for (int i = 0; i < test.length; i++) {
+			s += "[";
 			s += test[i];
-		}
-		while (unsorted) {
-			bs.sort(test);
-			String s1 = "";
-			for (int i = 0; i < test.length; i++) {
-				s += test[i];
-			}
-			if (s.equals(s1)) {
-				unsorted = false;
-			}
-			for (int i = 0; i < test.length; i++) {
-				s += test[i];
-			}
+			s += "] ";
 		}
 		System.out.println(s);
 	}
 
-	private void sort(int[] i) {
+	private static void sort(int[] i) {
 		for (int j = 1; j < i.length; j++) {
 			if (i[j] < i[j - 1]) {
 				int t = i[j - 1];
